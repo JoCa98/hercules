c898287eb4d572c94e3d97bfcf87e36fa7257209
ester2208 <maria.molinarichond@ucr.ac.cr>
2019-06-14 13:10:43 -0600

    footer fijado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 import ActCodeForm from './components/ActCodeForm';
 import PasswordRecovery from './components/PasswordRecovery';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <PasswordRecovery/>
+     
+      <Footer/>
       </div>
 
      
