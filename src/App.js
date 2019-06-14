5515f6dcf43b1fe370686738a5b34c3d3bbd504f
ester2208 <maria.molinarichond@ucr.ac.cr>
2019-06-14 11:50:59 -0600

    ester

--- src/App.js
+++ src/App.js
@@ -5,16 +5,11 @@ import PasswordRecovery from './components/PasswordRecovery';
 
 function App() {
   return (
-
-<<<<<<< HEAD
     <div className="App">
       <PasswordRecovery/>
       </div>
 
-=======
-        <EmailPruebaForm />
      
->>>>>>> d823291a5e6c93b12b0a88a8a9c3d8924a73ee72
   );
 }
 
--- src/components/PasswordRecovery.js
+++ src/components/PasswordRecovery.js
@@ -11,13 +11,19 @@ class PasswordRecovery extends Component {
                 <div className="row mt-4">
                     <div className="col-4 offset-4 card">
                         <form>
-                            <h1>Recuperación de Contraseña</h1>
+                            <h2 ><font color="#41ade7">Recuperación de Contraseña</font></h2>
+                            <br/>
                             <div class="form-group">
-                                <p align="justify">Ingrese el correo del usuario para recibir<br/>
-                                 una contraseña nueva temporal</p>
+                                <p align="justify">Ingrese el correo del usuario para recibir
+                                 una contraseña<br/> nueva temporal</p>
                                 <input type="text" name="emailText" className="form-control"></input>
                             </div>
-                            <button align="left" className="ConfirmButton">Confirmar</button>
+                            <div className="row">
+                                <div className="col-md-5 offset-md-7">
+                                <button align="left" className="ConfirmButton">Confirmar</button>
+                                </div>
+
+                            </div>
                         </form>
                     </div>
                 </div>
