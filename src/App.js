4b5be656fa91dbf686208a64d0f7be7adbf262f4
AntonyDJimenez <antony.jimenezgarcia@ucr.ac.cr>
2019-06-14 15:32:04 -0600

    lol

--- src/App.js
+++ src/App.js
@@ -7,30 +7,24 @@ import AddAdmin from './components/AddAdmin';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-<<<<<<< HEAD
+
 import Navbar from './components/Navbar';
-=======
+
 import EmailPruebaForm from './components/EmailPruebaForm';
 
->>>>>>> acfe4f4704e7f15d2cce29633885bb63a10dd01a
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <Header/>
       <Navbar/>
       <PasswordRecovery/>
       <Footer/>
-      </div>
 
-     
-=======
-      <Header />
-      <EmailPruebaForm />
-      <Footer />
     </div>
->>>>>>> acfe4f4704e7f15d2cce29633885bb63a10dd01a
+
   );
 }
 
