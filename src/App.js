d823291a5e6c93b12b0a88a8a9c3d8924a73ee72
AntonyDJimenez <antony.jimenezgarcia@ucr.ac.cr>
2019-06-14 11:29:36 -0600

    mejorado diseño del form

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ActCodeForm from './components/ActCodeForm';
+import EmailPruebaForm from './components/EmailPruebaForm';
 
 
 function App() {
   return (
 
-    <div className="App">
-
-      <header className="App-body">
-        <ActCodeForm />
-      </header>
-
-      <div className="App-body">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-      </div>
-    </div>
+        <EmailPruebaForm />
+     
   );
 }
 
