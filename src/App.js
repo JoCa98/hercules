3fb5945a371db310150f73212675b149eb43e6cd
AntonyDJimenez <antony.jimenezgarcia@ucr.ac.cr>
2019-06-16 19:35:40 -0600

    ...

--- src/App.js
+++ src/App.js
@@ -2,43 +2,27 @@ import React from 'react';
 import './App.css';
 import ActCodeForm from './components/ActCodeForm';
 import PasswordRecovery from './components/PasswordRecovery';
-
 import AddAdmin from './components/AddAdmin';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
-<<<<<<< HEAD
-
 import Navbar from './components/Navbar';
-
-=======
-import Navbar from './components/Navbar';
->>>>>>> c8c8b285b058449cb608e1ac8d58561279409045
 import EmailPruebaForm from './components/EmailPruebaForm';
 import HomeAdmin from './components/HomeAdmin';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> c8c8b285b058449cb608e1ac8d58561279409045
+/*sdsdsdsdsdsd */
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
 
-      <Header/>
-      <Navbar/>
-      <EmailPruebaForm/>
-      <Footer/>
+      <Header />
+      <Navbar />
+      <PasswordRecovery />
+      <Footer />
+
+      
 
     </div>
-
-=======
-
-      <HomeAdmin />
-    </div>
->>>>>>> c8c8b285b058449cb608e1ac8d58561279409045
   );
 }
 
