d0f7d995182a684f217f82433ff3ef6201d21bdf
ester2208 <maria.molinarichond@ucr.ac.cr>
2019-06-19 15:24:43 -0600

    añadir registro composición corporal

--- src/App.js
+++ src/App.js
@@ -13,23 +13,17 @@ import Navbar from './components/Navbar';
 import EmailPruebaForm from './components/EmailPruebaForm';
 import HomeAdmin from './components/HomeAdmin';
 import HistoricPhysicalInfo from './components/HistoricPhysicalInfo';
+import AddPhysicalInfo from './components/AddPhysicalInfo';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Header />
-      <HomeAdmin />
-      <Footer />
-    </div>
-=======
       <Header/>
       <Navbar/>
-      <HistoricPhysicalInfo/>
+     < AddPhysicalInfo/>
       <Footer/>
       </div>
 
->>>>>>> c1689f914778aebdc2f5838cb3eddbef53c4aa51
   );
 }
 
