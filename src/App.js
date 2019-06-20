67050cd7d58e24a768fbd5aef92ffcaf0d7b82e3
JoCa98 <44589247+JoCa98@users.noreply.github.com>
2019-06-19 19:16:27 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import PasswordRecovery from './components/PasswordRecovery';
 import AddAdmin from './components/AddAdmin';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import NavbarAdmin from './components/NavbarAdmin';
 import HomeAdmin from './components/HomeAdmin';
 import HistoricPhysicalInfo from './components/HistoricPhysicalInfo';
 import RoutineList from './components/RoutineList';
 import NavbarUserHome from './components/NavbarUserHome';
 import UserHome from './components/UserHome';
+
 
 function App() {
   return (
