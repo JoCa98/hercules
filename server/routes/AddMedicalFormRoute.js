337e9bc47aa0b800101a6b9f9aa818397580ee17
AntonyDJimenez <antony.jimenezgarcia@ucr.ac.cr>
2019-06-30 09:58:15 -0600

    se devuelven valores de la ruta (estado de la DB)

--- server/routes/AddMedicalFormRoute.js
+++ server/routes/AddMedicalFormRoute.js
@@ -11,7 +11,14 @@ router.post('/', function (req, res) {
   connection.query("CALL proc_addMedicalInfo(" + req.body.partyID + ",'" + req.body.date + "','" + req.body.pathologies + "','" + req.body.allergies + "','" + req.body.surgeries +
     "'," + req.body.traumas + "," + req.body.smoking + ",'" + req.body.neurologicalInfo + "','" + req.body.pulmonaryCardioInfo + "'," + req.body.bloodPressure +
     "," + req.body.heartRate + "," + req.body.heartRatePerMinute + "," + req.body.SpO2 + "," + req.body.weight + "," +
-    req.body.size + "," + req.body.IMC + "," + req.body.abdomen + "," + req.body.waist + "," + req.body.hip + ")");
+    req.body.size + "," + req.body.IMC + "," + req.body.abdomen + "," + req.body.waist + "," + req.body.hip + ")", function (err,result) {
+      if (err) {
+        return res.send(err)
+      }
+      else {
+        return res.send(result)
+      }
+    })
 
 });
 
