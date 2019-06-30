d32988de5d9a06744c2938bfb661a08ba7db1bfc
AntonyDJimenez <antony.jimenezgarcia@ucr.ac.cr>
2019-06-29 18:50:54 -0600

    ...

--- server/routes/AddMedicalFormRoute.js
+++ server/routes/AddMedicalFormRoute.js
@@ -9,12 +9,12 @@ router.use(cors());
 router.post('/', function (req, res) {
 
   connection.query("insert into medical_info(date, pathologies, allergies, surgeries, traumas,"
-   + "smoking, neurologicalInfo, pulmonaryCardioInfo, bloodPressure, heartRate, heartRatePerMinute, SpO2, partyID,"
-    + "weight, size, IMC, abdomen, waist, hip) values('" +req.body.date+"','"+req.body.pathologies+"','"+req.body.allergies+"','"+req.body.surgeries+
-    "',"+ req.body.traumas + ","+req.body.smoking+",'"+req.body.neurologicalInfo+"','"+req.body.pulmonaryCardioInfo+"',"+req.body.bloodPressure+
-    ","+req.body.heartRate+ ","+req.body.heartRatePerMinute+ ","+  req.body.SpO2+ ","+req.body.partyID+ 
-    ","+req.body.weight+ ","+req.body.size+ ","+req.body.IMC+ ","+req.body.abdomen+ ","+req.body.waist+ ","+req.body.hip+");")
-    
+    + "smoking, neurologicalInfo, pulmonaryCardioInfo, bloodPressure, heartRate, heartRatePerMinute, SpO2, partyID,"
+    + "weight, size, IMC, abdomen, waist, hip) values('" + req.body.date + "','" + req.body.pathologies + "','" + req.body.allergies + "','" + req.body.surgeries +
+    "'," + req.body.traumas + "," + req.body.smoking + ",'" + req.body.neurologicalInfo + "','" + req.body.pulmonaryCardioInfo + "'," + req.body.bloodPressure +
+    "," + req.body.heartRate + "," + req.body.heartRatePerMinute + "," + req.body.SpO2 + "," + req.body.partyID +
+    "," + req.body.weight + "," + req.body.size + "," + req.body.IMC + "," + req.body.abdomen + "," + req.body.waist + "," + req.body.hip + ");")
+
   console.log('Insertado')
 
 });
