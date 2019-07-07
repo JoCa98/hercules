4ff9e4a1793400e1fed62ef42b6f29d50c3dff1d
JoCa98 <44589247+JoCa98@users.noreply.github.com>
2019-07-07 15:23:03 -0600

    Home admin (listo) y ejemplo de documentación

--- server/routes/AdminRoute.js
+++ server/routes/AdminRoute.js
@@ -6,7 +6,7 @@ router.use(cors());
 
 
 router.get('/getUsersByCarnet',  (req, res) => {
-    connection.query("CALL proc_getUsersByCarnet('');", function(err,results){
+    connection.query("CALL proc_getUsersByCarnet('"+ req.query.carnet +"');", function(err,results){
         if (results) {
             res.send(results);
           }
@@ -14,7 +14,29 @@ router.get('/getUsersByCarnet',  (req, res) => {
             console.log(err);
           }
     });
-  
+  });
+
+  router.get('/getUsersByName',  (req, res) => {
+    connection.query("CALL proc_getUsersByName('"+ req.query.name +"');", function(err,results){
+        if (results) {
+            res.send(results);
+          }
+          else {
+            console.log(err);
+          }
+    });
+  });
+
+  router.get('/getUsersByIdentification',  (req, res) => {
+    connection.query("CALL proc_getUsersByIdentificationID('"+ req.query.identificationID +"');",
+     function(err,results){
+        if (results) {
+            res.send(results);
+          }
+          else {
+            console.log(err);
+          }
+    });
   });
 
 module.exports = router;
