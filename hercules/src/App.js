6670f01e4109a0d56ed690e2039f22fcb221cba4
JoCa98 <44589247+JoCa98@users.noreply.github.com>
2019-07-06 20:11:14 -0600

    HomeAdmin(avance2)

--- hercules/src/App.js
+++ hercules/src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="App">
       <Header />      
-      <HistoricMedicalInfo />
+      <HomeAdmin />
       <Footer />
     </div>
   );
--- hercules/src/components/HomeAdmin.js
+++ hercules/src/components/HomeAdmin.js
@@ -5,11 +5,14 @@ class HomeAdmin extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userList: [{}]
+            userList: [{}],
+            searchType: '5',
+            searchInput:'Hola'
         };
 
         this.getUserList = this.getUserList.bind(this);
-        this.redirect = this.redirect.bind(this);
+        this.rowEvent = this.rowEvent.bind(this);
+        this.handleInput = this.handleInput.bind(this);
     }
 
     componentDidMount() {
@@ -19,7 +22,6 @@ class HomeAdmin extends Component {
     getUserList() {
         try {
             axios.get(`http://localhost:9000/AdminRoute/getUsersByCarnet`).then(response => {
-                console.error(response.data);
                 const userList = response.data[0];
                 this.setState({ userList });
             });
@@ -27,9 +29,17 @@ class HomeAdmin extends Component {
             console.error(err);
         }
     }
-    redirect(event) {
+
+    handleInput(e) {
+        const { value, name } = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    rowEvent(event) {
         try {
-            alert(document.getElementById("myTable").rows[event.target.cellIndex].cells[0].innerHTML);
+            alert(document.getElementById("myTable").rows[event.target.parentNode.rowIndex].cells[0].innerHTML);
         } catch (err) {
             console.error(err);
         }
@@ -38,9 +48,9 @@ class HomeAdmin extends Component {
     render() {
         const userListVisual = this.state.userList.map((userList, i) => {
             return (
-                <tr className="pointer" onClick={this.redirect} key={i}>
+                <tr className="pointer" onClick={this.rowEvent} key={i}>
                     <td className="diplayNone">{userList.partyID}</td>
-                    <th scope="row">{userList.identificationID}</th>
+                    <td>{userList.identificationID}</td>
                     <td>{userList.fullName}</td>
                     <td>{userList.carnet}</td>
                 </tr>
@@ -53,15 +63,23 @@ class HomeAdmin extends Component {
                         <h1 className="text-left colorBlue">Lista de usuarios</h1>
                         <div className="row">
                             <div className="col-2 offset-1">
-                                <select className="form-control">
-                                    <option>Carnet</option>
-                                    <option>Nombre</option>
-                                    <option>Cedula</option>
+                                <select className="form-control"
+                                 name="searchType"
+                                 onChange={this.handleInput}>
+                                    <option value="0">Carnet</option>
+                                    <option value="1">Nombre</option>
+                                    <option value="2">Cedula</option>
                                 </select>
                             </div>
                             <div className="col-5">
-                                <input type="text" className="w-100 inputText" placeholder="Buscar"></input>
-                                <i className="fas fa-search" aria-hidden="true"></i>
+                                <input 
+                                type="text"
+                                name="searchInput" 
+                                onChange={this.handleInput}
+                                className="w-100 inputText" 
+                                placeholder="Buscar"
+                                >
+                                </input>
                             </div>
                             <div className="col-2">
                                 <button className="buttonSizeGeneral">Buscar</button>
@@ -72,8 +90,7 @@ class HomeAdmin extends Component {
                         <table className="table table-sm table-hover" id="myTable">
                             <thead>
                                 <tr>
-                                    <th scope="col" className="diplayNone">PartyID</th>
-                                    <th scope="col">Cedula</th>
+                                    <th scope="col">Cédula</th>
                                     <th scope="col">Nombre</th>
                                     <th scope="col">Carnet</th>
                                 </tr>
