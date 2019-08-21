23c5aa1f3200c44d0ecc164f9dfc3201c79b6b18
JoCa98 <44589247+JoCa98@users.noreply.github.com>
2019-08-21 16:37:54 -0600

    navbar

--- hercules/src/components/NavbarUserHome.js
+++ hercules/src/components/NavbarUserHome.js
@@ -4,21 +4,39 @@ import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
 class NavbarUserHome extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.logOut = this.logOut.bind(this);
+        this.userConfiguration = this.userConfiguration.bind(this);
+        this.medicalUserInfo = this.medicalUserInfo.bind(this);
+        this.physicalUserInfo = this.physicalUserInfo.bind(this);
     }
 
-    logOut(){
+    logOut() {
         this.props.history.push(`/`);
         window.location.reload();
         sessionStorage.clear();
     }
+
+    userHome() {
+        this.props.history.push(`/UserHome`);
+    }
+
+    userConfiguration() {
+        this.props.history.push(`/UserConfiguration`);
+    }
+
+    medicalUserInfo() {
+        this.props.history.push(`/HistoricMedicalUserInfo`);
+    }
+
+    physicalUserInfo() {
+        this.props.history.push(`/HistoricMedicalUserInfo`);
+    }
     render() {
         return (
-            <div className="container-fluid navbarColor">
-                <div className="row ">
-                    <div className="col-12 text-center">
+           
+               
                         <nav className="navbar navbar-expand-md navbarColor justify-content-end">
                             <button class="navbar-toggler icon-bar navbar-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
                                 <span class="navbar-toggler-icon "></span>
@@ -26,27 +44,26 @@ class NavbarUserHome extends Component {
                             <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
                                 <ul className="navbar-nav text-center">
 
-                                    <li className="nav-item active ml-4 mt-1">
-                                        <Link to="/UserHome" className="align-middle">Rutina</Link>
+                                    <li className="nav-item">
+                                        <button className="btn buttonNavbar" onClick={this.userHome}>Rutina</button>
                                     </li>
-                                    <li className="nav-item ml-4 mt-1">
-                                        <Link to="/HistoricMedicalUserInfo" className="align-middle">Consulta médica</Link>
+                                    <li className="nav-item">
+                                        <button className="btn buttonNavbar" onClick={this.medicalUserInfo}>Consulta médica</button>
                                     </li>
-                                    <li className="nav-item ml-4 mt-1">
-                                        <Link to="/HistoricPhysicalUserInfo" className="align-middle">Composición corporal</Link>
+                                    <li className="nav-item">
+                                        <button className="btn buttonNavbar" onClick={this.physicalUserInfo}>Composición corporal</button>
                                     </li>
-                                    <li className="nav-item ml-4 mt-1">
-                                        <Link to="/UserConfiguration" className="align-middle">Perfil</Link>
+                                    <li className="nav-item">
+                                        <button className="btn buttonNavbar" onClick={this.userConfiguration}>Perfil</button>
                                     </li>
-                                    <li className="nav-item ml-4 mt-1">
-                                        <Link to="/" className="align-middle" onClick={this.logOut}>Salir</Link>
+                                    <li className="nav-item">
+                                        <button className="btn buttonNavbar" onClick={this.logOut}>Salir</button>
                                     </li>
                                 </ul>
                             </div>
                         </nav>
-                    </div>
-                </div>
-            </div>
+                   
+            
 
         )
     }
