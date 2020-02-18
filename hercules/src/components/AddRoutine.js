192012e1b2d4fb1b379c2cbd0f5a8fde6f7e3b6c
ester2208 <maria.molinarichond@ucr.ac.cr>
2020-02-18 11:41:09 -0600

    .

--- hercules/src/components/AddRoutine.js
+++ hercules/src/components/AddRoutine.js
@@ -603,7 +603,6 @@ class AddRoutine extends Component {
      */
     handleSubmit(e) {
         var id;
-        if (!this.empty()) {
             axios.post("http://localhost:9000/RoutineRoute/addRoutine", {
                 Frecuency: this.state.Frecuency,
                 Intensity: this.state.Intensity,
@@ -623,12 +622,8 @@ class AddRoutine extends Component {
                 .catch(err => console.error(err));
 
             e.preventDefault();
-        } else {
-            this.modalTrigger(e, 'Campos obligatorios', 'Debe agregar los datos de la preescripción física');
-            e.preventDefault();
-        }
-
-    }
+        } 
+    
 
     /**
      * Method that add a list of exercises to a routine in the database
@@ -674,8 +669,8 @@ class AddRoutine extends Component {
      * Method that verifies if some input is empty
      */
     empty() {
-        if (this.state.Frecuency == "" || this.state.Density == "" || this.state.Intensity == "" || this.state.RestTime == ""
-            || this.state.objectiveID == "" || this.state.routineTypeID == "" || this.state.HeartRatePerMinute == "") {
+        if (this.state.Frecuency.toString().trim() == 0 || this.state.Density.toString().trim() == 0 || this.state.Intensity.toString().trim() == 0 || this.state.RestTime.toString().trim() == 0 
+            || this.state.objectiveID.toString().trim() == 0 || this.state.routineTypeID.toString().trim() == 0 || this.state.HeartRatePerMinute.trim() == 0) {
             return true;
         } else {
             return false;
