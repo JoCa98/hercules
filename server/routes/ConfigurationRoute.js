f40cc10616328964adfd08aff5ae142667977b5b
Victor Hugo Bolaños <victor.bolanoscorrales@ucr.ac.cr>
2020-05-25 10:20:36 -0600

    API's agregar/editar ejercicios
    Se añadio las API's de agregar y editar ejercicios.

--- server/routes/ConfigurationRoute.js
+++ server/routes/ConfigurationRoute.js
@@ -15,4 +15,26 @@ router.post('/AddCareer',(req,res) => {
     });
 });
 
+router.post('/AddExcercise',(req,res) => {
+  connection.query("CALL proc_addNewExercise('" + req.body.exerciseDescription + "','" + req.body.imagePath + "','" + req.body.exerciseLink + "','" + req.body.exerciseTypeID + "');", function(err, results) {
+      if (results) {
+          res.send(results);
+        }
+        else {
+          console.log(err);
+        }
+  });
+});
+
+router.post('/EditExcercise',(req,res) => {
+  connection.query("CALL proc_updateExercise('" + req.body.exerciseID + "','" + req.body.exerciseDescription + "','" + req.body.exerciseLink + "','" + req.body.exerciseTypeID + "');", function(err, results) {
+      if (results) {
+          res.send(results);
+        }
+        else {
+          console.log(err);
+        }
+  });
+});
+
 module.exports = router;
