4626f7e21b102c515296760f2ab19d27494026ac
Victor Hugo Bolaños <victor.bolanoscorrales@ucr.ac.cr>
2020-06-09 20:33:28 -0600

    SP, PaginaConfiguracion y Metodos PaginaConfig
    Se agrego SP de elminiar admins
    Se agrego pagina de configuracion
    Se agreagaron los metodos de pagina de configuracion

--- hercules/src/components/Configuration.js
+++ hercules/src/components/Configuration.js
@@ -22,112 +22,26 @@ class Configuration extends Component {
             permissionsManager: new PermissionsManager(),
             validations: new validations(),
             userTypeID: "3",
-            careerName: null,
-            userTypeList: [],
             show: false,
-            modalTittle: "",
-            modalChildren: "",
             isExit: false
         };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.empty = this.empty.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
         this.careerConfiguration = this.careerConfiguration.bind(this);
         this.excerciseConfiguration = this.excerciseConfiguration.bind(this);
         this.accountConfiguration = this.accountConfiguration.bind(this);
-        this.getAdminUserType = this.getAdminUserType.bind(this);
-        this.modalTrigger = this.modalTrigger.bind(this);
-        this.closeModal = this.closeModal.bind(this);
+
     }
 
     componentDidMount() {
         /* if (this.state.permissionsManager.validatePermission(this.props.location.pathname, this)) {*/
         window.scrollTo(0, 0);
-        this.getAdminUserType();
+        //this.getAdminUserType();
         /*}*/
     }
 
     /**
-        * Method that submit all the information in the form to the database
-        */
-    handleSubmit = event => {
-
-        axios.get(`http://localhost:9000/User/isEmailValid`, { params: { email: this.state.email } }).then(response => {
-            var isEmailValid = JSON.parse(JSON.stringify(response.data))[0]['isEmailValid'].data[0];
-
-            if (this.empty()) {
-                this.modalTrigger(event, 'Campos obligatorios', 'Los campos de texto con un * no se pueden dejar en blanco');
-            } else if (!this.state.validations.validateTextField(this.state.careerName.trim())) {
-                this.modalTrigger(event, 'Nombre de la carrera', 'El nombre de la carrera solo pueden estar compuesto por letras');
-            }
-        });
-        event.preventDefault();
-    }
-
-    /**
-     * This method takes care of show a modal with useful information
-     */
-    modalTrigger(event, mdTittle, mdChildren) {
-        this.setState({
-            show: !this.state.show,
-            modalTittle: mdTittle,
-            modalChildren: mdChildren
-        });
-        event.preventDefault();
-    };
-
-    /**
-     * This method close the modal  
-     */
-    closeModal(event) {
-        this.setState({
-            show: !this.state.show
-        });
-        if (this.state.isExit) {
-            this.props.history.push(`/HomeAdmin`);
-        }
-        event.preventDefault();
-    };
-
-    /**
-    * This method set the prop attributes
+    * Method that redirect to the requested page
     */
-    handleInputChange(event) {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-    }
-
-    /**
-    * This method load the information in the dropdownlist
-    */
-    getAdminUserType() {
-        try {
-            axios.get(`http://localhost:9000/AdminRoute/getAdminUserType`).then(response => {
-                const userTypeList = response.data[0];
-                this.setState({ userTypeList });
-            });
-        } catch (err) {
-            console.error("Un error inesperado a ocurrido");
-        }
-    }
-
-    /**
-    * Method that verify that the require inputs are not empty
-    */
-    empty() {
-        if (this.state.careerName == "" || this.state.careerName == null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-* Method that redirect to the previous page
-*/
     careerConfiguration() {
         this.props.history.push(`/CareerConfiguration`);
     }
@@ -137,26 +51,10 @@ class Configuration extends Component {
     }
 
     accountConfiguration() {
-        this.props.history.push(`/HomeAdmin`);
+        this.props.history.push(`/AccountConfiguration`);
     }
 
     render() {
-        const selectUserType = this.state.userTypeList.map((userTypeList, i) => {
-            if (i == 0) {
-                return (
-
-                    <option defaultValue={userTypeList.userTypeID}
-                        value={userTypeList.userTypeID}>{userTypeList.description}</option>
-
-                )
-            } else {
-                return (
-                    <option value={userTypeList.userTypeID}>{userTypeList.description}</option>
-
-                )
-            }
-
-        })
         return (
             <div className="container">
                 <div className="row mt-4">
--- server/routes/ConfigurationRoute.js
+++ server/routes/ConfigurationRoute.js
@@ -111,5 +111,107 @@ router.get('/getExercisesByNameAndType', (req, res) => {
   });
 });
 
+/////////
+
+router.post('/AddExcercise',(req,res) => {
+  connection.query("CALL proc_addNewExercise('" + req.body.exerciseDescription + "','" + req.body.exerciseLink + "'," + req.body.exerciseTypeID + ");", function(err, results) {
+      if (results) {
+          res.send(results);
+        }
+        else {
+          console.log(err);
+        }
+  });
+});
+
+router.post('/EditExcercise',(req,res) => {
+  connection.query("CALL proc_updateExercise('" + req.body.exerciseID + "','" + req.body.exerciseDescription + "','" + req.body.exerciseLink + "'," + req.body.exerciseTypeID + ");", function(err, results) {
+      if (results) {
+          res.send(results);
+        }
+        else {
+          console.log(err);
+        }
+  });
+});
+
+router.post('/ExerciseStatus',(req,res) =>{
+  connection.query("CALL proc_changeExerciseStatus(" + req.body.exerciseID + "," + req.body.status + ");", function(err,results){
+    if (results) {
+      res.send(results);
+    }
+    else {
+      console.log(err);
+    }
+  });
+});
+
+//Cuentas
+router.get('/isMasterEmail', (req, res) => {
+  connection.query("Select fun_verifyMasterAdmin('" + req.query.email + "') AS masterEmail", function (err, results) {
+    if (results) {
+      res.send(results);
+    }
+    else {
+      console.log(err);
+    }
+  });
+});
+
+router.get('/AdminAccounts', (req, res) => {
+  connection.query("CALL proc_getAllGymAdmin()", function (err,results){
+    if (results) {
+      res.send(results);
+    }
+    else {
+      console.log(err);
+    }
+ });
+});
+
+router.get('/MedicalAccounts', (req, res) => {
+  connection.query("CALL proc_getAllMedicalAdmin()", function (err,results){
+    if (results) {
+      res.send(results);
+    }
+    else {
+      console.log(err);
+    }
+ });
+});
+
+router.get('/InactiveMedics', (req, res) => {
+  connection.query("CALL proc_getInactiveMedics()", function (err,results){
+    if (results) {
+      res.send(results);
+    }
+    else {
+      console.log(err);
+    }
+ });
+});
+
+router.get('/ChangeMedicStatus', (req, res) => {
+  connection.query("CALL proc_changeMedicStatus('"+ req.query.email +"'," + req.query.status + ")", function (err,results){
+    if (results) {
+      res.send(results);
+    }
+    else {
+      console.log(err);
+    }
+ });
+});
+
+router.get('/DeleteAdmin', (req, res) => {
+  connection.query("CALL proc_deleteAdmin('"+ req.query.email +"')", function (err,results){
+    if (results) {
+      res.send(results);
+    }
+    else {
+      console.log(err);
+    }
+ });
+});
+
 module.exports = router;
 
